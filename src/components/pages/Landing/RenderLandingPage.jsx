4a2f0e569af6c7772349c5d04a4b2e60f9b888b1
ashtilawat23 <ashtilawat23@gmail.com>
2023-02-01 09:55:39 -0500

    first ticket ready

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+// ADD IMPORTS BACK FOR GRAPHS SECTION
+// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -30,32 +31,8 @@ function RenderLandingPage(props) {
         </div>
       </div>
 
-      <div className="graphs-section">
-        <div className="grant-rates-by-office-graph-container">
-          <img
-            src={GrantRatesByOfficeImg}
-            alt="Grant Rates By Office Graph"
-            className="gr-office-img"
-          />
-          <p>Search Grant Rates By Office</p>
-        </div>
-        <div className="grant-rates-by-nationality-container">
-          <img
-            src={GrantRatesByNationalityImg}
-            alt="Grant Rates By Nationality Graph"
-            className="gr-nationality-img"
-          />
-          <p>Search Grant Rates By Nationality</p>
-        </div>
-        <div className="grant-rates-over-time-container">
-          <img
-            src={GrantRatesOverTimeImg}
-            alt="Grant Rates Over Time Graph"
-            className="gr-overtime-img"
-          />
-          <p>Search Grant Rates Over Time</p>
-        </div>
-      </div>
+      {/* Graphs Section: Add code here for the graphs section for your first ticket */}
+      {/* <div className="graphs-section"> */}
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -82,45 +59,9 @@ function RenderLandingPage(props) {
           </h3>
         </div>
       </div>
-
-      <div className="bottom-section">
-        <h1>Systemic Disparity Insights</h1>
-        <div className="data-container">
-          <div className="first-data-point-container">
-            <h2>36%</h2>
-            <h3>
-              By the end of the Trump administration, the average asylum office
-              grant rate had fallen 44 percent in fiscal year 2016 to 28 percent
-              in fiscal year 2020.
-            </h3>
-          </div>
-          <div className="second-data-point-container">
-            <h2>5%</h2>
-            <h3>
-              The New York asylum office grant rate dropped to 5 percent in
-              fiscal year 2020
-            </h3>
-          </div>
-          <div className="third-data-point-container">
-            <h2>6x Lower</h2>
-            <h3>
-              Between fiscal year 2017 and 2020, the New York asylum office’s
-              average grant rate was six times lower than the San Francisco
-              asylum office
-            </h3>
-          </div>
-        </div>
-        <a href="https://www.humanrightsfirst.org/resource/uscis-records-reveal-systemic-disparities-asylum-decisions">
-          <div className="read-more-btn">
-            <Button
-              type="default"
-              style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
-            >
-              Read More
-            </Button>
-          </div>
-        </a>
-
+      <div>
+        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+        {/* <div className="bottom-section">*/}
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
---
+++ tickets/firstTicket.md
@@ -0,0 +1,35 @@
+# Build Sprint 1
+
+To begin work on this ticket, make sure you have finished: 
+1. Getting locally setup. 
+2. Completed the onboarding module on Canvas. 
+
+## Objective
+
+Deliver the following: 
+- Landing page that matches the landing page on the [deployed site](https://asylum-rg-fe.vercel.app/). 
+- While you may make small changes to styles, the page should mostly match what is provided. 
+
+## Relevant Files
+
+Your React code should be added to `RenderLandingPage.jsx`. 
+Your styles should be added to `RenderLandingPage.less`.
+
+There are comments in both files to guide you and show you where to add code. 
+
+## Guidance
+
+Your job is to finish building the landing page for this application. To build this, you will add to the existing React component structure and alter styles to match the provided reference on the deployed site. 
+
+You should make sure: 
+1. You are focusing on making the landing page for the browser. You can adjust the styles for mobile and tablet in the stretch goals of this project after you complete all 3 tickets.
+2. You use the exact imports on the `RenderLandingPage.jsx` file. This way you have the same photos and libraries available to you as the deployed site. 
+3. You are not using a node version higher than 16.16.0. If you are, you can use `nvm` (for Mac) or `nvm-windows` to easily toggle your node versions in directories. 
+4. It is important to make this project your own, but keep the integrity of the main files so that the app can render and work when you wish to present it. 
+
+## Deliverables 
+
+Submit the following on Canvas: 
+- Link to your forked repo with the added code for the landing page
+- Link to a Loom video answering the prompt in the Canvas assignment
+
